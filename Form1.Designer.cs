72ec727de3c936f943155fe9bdc8453363856630
balybin <balybin.iwan@yandex.ru>
2017-12-12 17:44:41 +0700

    blablabla

---
+++ Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace zmeya
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.управлениеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.number = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.X = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(579, 587);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.управлениеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(902, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSeparator1,
+            this.открытьToolStripMenuItem,
+            this.сохранитьToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(48, 20);
+            this.toolStripMenuItem1.Text = "Файл";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(129, 6);
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // управлениеToolStripMenuItem
+            // 
+            this.управлениеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выходToolStripMenuItem1});
+            this.управлениеToolStripMenuItem.Name = "управлениеToolStripMenuItem";
+            this.управлениеToolStripMenuItem.Size = new System.Drawing.Size(85, 20);
+            this.управлениеToolStripMenuItem.Text = "Управление";
+            // 
+            // выходToolStripMenuItem1
+            // 
+            this.выходToolStripMenuItem1.Name = "выходToolStripMenuItem1";
+            this.выходToolStripMenuItem1.Size = new System.Drawing.Size(108, 22);
+            this.выходToolStripMenuItem1.Text = "Выход";
+            this.выходToolStripMenuItem1.Click += new System.EventHandler(this.выходToolStripMenuItem1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.number,
+            this.X,
+            this.Y});
+            this.dataGridView1.Location = new System.Drawing.Point(587, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(303, 247);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellParsing += new System.Windows.Forms.DataGridViewCellParsingEventHandler(this.dataGridView1_CellParsing);
+            this.dataGridView1.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.dataGridView1_EditingControlShowing);
+            // 
+            // number
+            // 
+            this.number.Frozen = true;
+            this.number.HeaderText = "№";
+            this.number.Name = "number";
+            this.number.ReadOnly = true;
+            this.number.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // X
+            // 
+            this.X.DividerWidth = 1;
+            this.X.Frozen = true;
+            this.X.HeaderText = "X";
+            this.X.Name = "X";
+            this.X.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // Y
+            // 
+            this.Y.DividerWidth = 1;
+            this.Y.Frozen = true;
+            this.Y.HeaderText = "Y";
+            this.Y.Name = "Y";
+            this.Y.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(902, 616);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem управлениеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn number;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Y;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace zmeya
+{
+    public partial class Form1 : Form
+    {
+        int KeysBufMaxSize, SpeedOutput;
+        List<int> KeysBuffer;
+        Graphics draw;
+        SolidBrush whiteBrush;
+        SolidBrush orangeBrush;
+        SolidBrush redBrush;
+        Pen redPen;
+        Pen blackPen;
+        bool isFileLoad = false;
+        bool reDrow = false;
+        public Form1()
+        {
+            InitializeComponent();
+            whiteBrush = new SolidBrush(Color.White);
+            orangeBrush = new SolidBrush(Color.Orange);
+            redBrush = new SolidBrush(Color.Red);
+            redPen = new Pen(Color.Red);
+            blackPen = new Pen(Color.Black);
+            KeysBufMaxSize = 2;
+            KeysBuffer = new List<int>();
+            draw = pictureBox1.CreateGraphics();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            if (KeysBuffer.Count >= KeysBufMaxSize)
+            {
+                return;
+            }
+            int direction = -1;
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    direction = 0;
+                    break;
+                case Keys.Right:
+                    direction = 1;
+                    break;
+                case Keys.Down:
+                    direction = 2;
+                    break;
+            }
+        }
+
+        private void выходToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result;
+            result = MessageBox.Show("＜￣｀ヽ、　　　　　　　／￣＞\n　ゝ、　　＼　／⌒ヽ, ノ /´\n　　　ゝ、　` //＼° ͜ʖ ͡°/ ／\n　　 　　>　 　 　  , ノ\n　　　　　∠_,,,/´", "улетаешь без сохранения?", buttons);
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void dataGridView1_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            TextBox tb = (TextBox)e.Control;
+            tb.KeyPress += new KeyPressEventHandler(tb_KeyPress);
+
+
+        }
+
+
+        void tb_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string vlCell = ((TextBox)sender).Text;
+            if (!(Char.IsDigit(e.KeyChar)) && !((e.KeyChar == '.')
+                && (vlCell.IndexOf(".") == -1) && (vlCell.Length != 0)) 
+                &&!((e.KeyChar == '-') && (vlCell.Length == 0)))
+            {
+                if (e.KeyChar != (char)Keys.Back)
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                FileStream stream = new FileStream(saveFileDialog.FileName, FileMode.Create, FileAccess.Write);
+                StreamWriter writer = new StreamWriter(stream, System.Text.Encoding.Unicode);
+                for (int i = 0; i < dataGridView1.Rows.Count; ++i)
+                {
+                    if (dataGridView1[1,i].Value != null && dataGridView1[2,i].Value != null)
+                        writer.WriteLine(dataGridView1[1,i].Value.ToString() + " " + dataGridView1[2,i].Value.ToString());
+                }
+                writer.Close();
+                stream.Close();
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (reDrow)
+            {
+                e.Graphics.FillRectangle(whiteBrush, 0, 0, pictureBox1.Width, pictureBox1.Height);
+                for (int i = 0; i < dataGridView1.Rows.Count - 2; ++i)
+                {
+                    e.Graphics.DrawLine(blackPen,
+                        float.Parse(dataGridView1[1, i].Value.ToString(), System.Globalization.CultureInfo.InvariantCulture),
+                        float.Parse(dataGridView1[2, i].Value.ToString(), System.Globalization.CultureInfo.InvariantCulture),
+                        float.Parse(dataGridView1[1, i + 1].Value.ToString(), System.Globalization.CultureInfo.InvariantCulture),
+                        float.Parse(dataGridView1[2, i + 1].Value.ToString(), System.Globalization.CultureInfo.InvariantCulture));
+                }
+            }
+        }
+
+        private void dataGridView1_CellParsing(object sender, DataGridViewCellParsingEventArgs e)
+        {
+            for (int i = 0; i < dataGridView1.Rows.Count; ++i)
+            {
+                if (dataGridView1[1, i] != null)
+                    if(dataGridView1[1, i].Value.ToString().Length == 0)
+                {
+                    dataGridView1.Rows.RemoveAt(i);
+                    for (int j = i; j < dataGridView1.Rows.Count; ++j)
+                        dataGridView1[0, j].Value = (int.Parse(dataGridView1[0, j].Value.ToString()) - 1).ToString();
+
+                    --i;
+                }
+            }
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (isFileLoad)
+            {
+                dataGridView1.Rows.Clear();
+            }
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() != DialogResult.OK) return;
+            StreamReader reader = new StreamReader(openFileDialog1.FileName);
+            string buf = reader.ReadLine();
+            string[] result;
+            int point = 1;
+            //try
+            //{
+            while (buf != null)
+            {
+                buf = point.ToString() + " " + buf;
+                result = buf.Split(' ');
+                dataGridView1.Rows.Add(result);
+                buf = reader.ReadLine();
+                ++point;
+            }
+            isFileLoad = true;
+            reDrow = true;
+                //}
+                // catch()
+             pictureBox1.Refresh();
+        }
+    }
+   
+}
